2a42ff176ec95f87e0edfe504fd76ddb90ee7c10
Abiodun Biobaku <biolafrica@gmail.com>
2025-03-13 16:02:55 +0000

    created footer component

--- app/components/nav/footer.js
+++ app/components/nav/footer.js
@@ -1,6 +1,38 @@
+import Link from "next/link"
+
+
+
 export default function Footer(){
+  const year = new Date().getFullYear();
+
   return(
-    <></>
+    <>
+      <div className="footer">
+
+        <h3><b>Keep in Touch</b></h3>
+        <h5>I am currently specializing in MERN stack.</h5>
+        <h5>Lets Connect and Build Something Great Together.</h5>
+
+        <div className="footer-btn">
+
+          <Link href="mailto:biolafrica@gmail.com" className="pri-btn" target="_blank">
+            <img src="icons/Email.svg" alt="" />
+            <h6>Email</h6>
+          </Link>
+
+          <Link href="https://www.linkedin.com/in/abiobaku/" className="sec-btn" target="_blank">
+            <img src="icons/LinkedIn.svg" alt="" />
+            <h6>LinkedIn</h6>
+          </Link>
+
+        </div>
+      </div>
+
+      <div className="copyright">
+        <h6 class="dark-color">Copyright &copy; {year} Abiodun Biobaku</h6>
+      </div>
+    </>
+  
   )
 
 }
