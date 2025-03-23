014418873e85820a61734b837aa138d23c10b55f
Abiodun Biobaku <biolafrica@gmail.com>
2025-03-23 01:15:05 +0000

    implemented  loading component

---
+++ app/loading.jsx
@@ -0,0 +1,8 @@
+export default function Loading(){
+  return(
+    <main className="loading">
+      <h2 style={{color: "#3ecf8e"}}>Loading....</h2>
+      <p>Hopefully not for too long :)</p>
+    </main>
+  )
+}
